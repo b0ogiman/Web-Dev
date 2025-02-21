1e216de0d50c857750e2b929e819307f178726ca
b0ogiman <158202661+b0ogiman@users.noreply.github.com>
2025-02-21 21:56:24 +0500

    Add files via upload

---
+++ product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CommonModule, ProductCardComponent], // Добавляем компонент карточки
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+  }
+}
+
